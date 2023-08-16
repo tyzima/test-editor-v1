3605a22f6e5eedb0842ff4484fac690e33c91542
tyzima <41972512+tyzima@users.noreply.github.com>
2023-08-16 17:42:55 -0400

    Update demoContent.js

--- src/utils/demoContent.js
+++ src/utils/demoContent.js
@@ -23,27 +23,27 @@ if (params.has('image')) {
     const imageUrl = params.get('image');
     fabric.Image.fromURL(imageUrl, function(oImg) {
         
-        // Set height to 400px and adjust width to maintain aspect ratio
-        const scaleFactor = 400 / oImg.height;
+        const widthScaleFactor = canvas.width / oImg.width;
+        const heightScaleFactor = canvas.height / oImg.height;
+        const scaleFactor = Math.min(widthScaleFactor, heightScaleFactor);
+
         oImg.scale(scaleFactor);
-        
+
         // Adjust the position
         oImg.set({
-            left: (canvas.width - oImg.width * oImg.scaleX) / 2 - 120, // Subtract 120 from the horizontal centering
+            left: (canvas.width - oImg.width * oImg.scaleX) / 2 - 120,
             top: (canvas.height - oImg.height * oImg.scaleY) / 2
         });
 
-        // Add the image to the canvas
         canvas.add(oImg);
-        
-        // Set the image as the active selection
         canvas.setActiveObject(oImg);
-
+        
         // Use the alignObject function to center the image vertically
         alignObject('center-v');
 
     }, { crossOrigin: 'Anonymous' });
 }
+
 
   // Check if an "svg" parameter exists
 if (params.has('svg')) {
@@ -51,10 +51,10 @@ if (params.has('svg')) {
     fabric.loadSVGFromURL(svgUrl, function(objects) {
         const obj = fabric.util.groupSVGElements(objects);
 
-        // Scale if necessary (similar to the image scaling logic, if needed)
-        // Here, I'm just replicating the image's scaling logic for reference. 
-        // If your SVG doesn't need to be 400px tall, you can adjust or remove this.
-        const scaleFactor = 400 / obj.height;
+        const widthScaleFactor = canvas.width / obj.width;
+        const heightScaleFactor = canvas.height / obj.height;
+        const scaleFactor = Math.min(widthScaleFactor, heightScaleFactor);
+
         obj.scale(scaleFactor);
 
         // Adjust the position
@@ -71,6 +71,7 @@ if (params.has('svg')) {
 
     });
 }
+
 
 }
 
