bd09deca78dd706a59c91bf09f014512a633e6d2
tyzima <41972512+tyzima@users.noreply.github.com>
2023-08-16 16:55:17 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import './Header.scss';
 const Header = (props) => {
   return (
     <header>
-      <img src= 'https://res.cloudinary.com/laxdotcom/image/upload/v1692218727/Asset_1li_k9ocb8.svg' alt="logo" />
+      <img src= 'https://res.cloudinary.com/laxdotcom/image/upload/v1692218727/Asset_1li_k9ocb8.svg' alt="logo" width="150" height="auto" />
       <div className="menu">{props.children}</div>
     </header>
     )
