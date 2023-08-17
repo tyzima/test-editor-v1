bfff67ee548c1225b6b58cbf915dc1ef328a78be
tyzima <41972512+tyzima@users.noreply.github.com>
2023-08-16 22:45:15 -0400

    Update SelectionColorSettings.js

--- src/components/SelectionColorSettings.js
+++ src/components/SelectionColorSettings.js
@@ -1,43 +1,50 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { colors } from './colors';  // Assuming colors.js is in the same directory
 
-import React, { useState, useContext } from "react";
-import { FabricContext } from "./FabricContext";
-import { predefinedColors } from "./colorArray";
+const SelectionColorSettings = ({ fabricCanvas }) => {
+    const [selectedColors, setSelectedColors] = useState([]);
 
-function SelectionColorSettings() {
-    const [fabricCanvas, setFabricCanvas] = useContext(FabricContext);
-    const [selectedColor, setSelectedColor] = useState("");
-
-    const handleColorChange = (color) => {
-        setSelectedColor(color);
+    useEffect(() => {
+        // Logic to determine the colors of the selected objects on the canvas
+        // and set them to the selectedColors state.
+        // For simplicity, this assumes the SVG objects have a 'fill' property.
         if (fabricCanvas) {
             const activeObjects = fabricCanvas.getActiveObjects();
-            activeObjects.forEach(obj => {
-                if (obj.setFill) {
-                    obj.setFill(color);
-                    fabricCanvas.renderAll();
-                }
-            });
+            const activeColors = activeObjects.map(obj => obj.fill);
+            setSelectedColors([...new Set(activeColors)]); // Remove duplicates
         }
+    }, [fabricCanvas]);
+
+    const handlePredefinedColorSelect = (color) => {
+        const activeObjects = fabricCanvas.getActiveObjects();
+        
+        activeObjects.forEach(object => {
+            object.set('fill', color);
+        });
+
+        fabricCanvas.renderAll();
     };
 
     return (
-        <div className="setting">
-            <div className="label">Color</div>
-            <div className="function">
-                <div className="predefined-colors">
-                    {predefinedColors.map((colorObj) => (
-                        <div
-                            key={colorObj.name}
-                            className="color-swatch"
-                            style={{ backgroundColor: colorObj.color }}
-                            onClick={() => handleColorChange(colorObj.color)}
-                            title={colorObj.name}
-                        ></div>
-                    ))}
-                </div>
+        <div className="color-settings">
+            <div className="selected-colors">
+                {selectedColors.map((color, index) => (
+                    <div key={index} style={{ backgroundColor: color }}></div>
+                ))}
+            </div>
+            <div className="predefined-colors">
+                {colors.map((color, index) => (
+                    <div
+                        key={index}
+                        style={{ backgroundColor: color.hex }}
+                        onClick={() => handlePredefinedColorSelect(color.hex)}
+                    >
+                        {color.name}
+                    </div>
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default SelectionColorSettings;
